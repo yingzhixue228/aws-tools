c7e91a1117936e750a6ee682d8e90b1fb533416d
Serdar Tumgoren <tumgorenz@washpost.com>
2011-12-03 16:52:05 -0500

    initial setup.py commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='aws-tools',
+      version='0.1',
+      description='Basic helpers for Amazon Web Services',
+      author='Serdar Tumgoren',
+      author_email='zstumgoren@gmail.com',
+      url='https://github.com/zstumgoren/aws-tools',
+      license='MIT',
+      packages=['aws'],
+      scripts=['scripts/s3_upload.py'],
+      requires=['boto'],
+      classifiers=[
+          'Environment :: Web Environment',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python',
+          'Topic :: Software Development :: Libraries',
+          'Topic :: Utilities'
+      ]
+)
